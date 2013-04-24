031f7e655accca4cd1cb9c15d7a9543393237264
myjimnelson <jim@jimnelson.us>
2013-04-23 19:56:16 -0500

    Moved all files into repo, using Google-hosted link for jQuery

---
+++ html/map.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {          // when document ready...
+
+  //$(".tile").addClass("fog");
+  //$("div:contains('0x8')").addClass("fog");
+  //$("div:contains('0x8')").css("color", "blue");
+  //$("div.visible:contains('0x8')").css("color", "blue");
+  //$("div.visible:contains('0x8')").css("background-color", "darkblue");
+  //$("div.visible:contains('0x6')").css("background-color", "blue");
+  //$("div.visible:contains('0x0')").css("background-color", "lightblue");
+
+  //$("td.visible:contains('0x6')").css("background-color", "blue");
+  $("td.visible:contains('0x0')").css("color", "white");
+
+  $("td.visible:contains('0xdd')").css("background-color", "darkblue");
+  $("td.visible:contains('0xcc')").css("background-color", "blue");
+  $("td.visible:contains('0xbb')").css("background-color", "lightblue");
+  $("td.visible:contains('0x0')").css("background-color", "cornsilk");
+  $("td.visible:contains('0x33')").css("background-color", "white");
+  $("td.visible:contains('0x11')").css("background-color", "sandybrown");
+  $("td.visible:contains('0x62')").css("background-color", "chocolate");
+
+  $("td.visible:contains('0xdd')").css("color", "darkblue");
+  $("td.visible:contains('0xcc')").css("color", "blue");
+  $("td.visible:contains('0xbb')").css("color", "lightblue");
+  $("td.visible:contains('0x0')").css("color", "cornsilk");
+  $("td.visible:contains('0x33')").css("color", "white");
+  $("td.visible:contains('0x11')").css("color", "sandybrown");
+  $("td.visible:contains('0x62')").css("color", "chocolate");
+
+  $("td.fog").css("color", "black");
+});
