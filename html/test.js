a203bb5ceaf2b1e1f158780d8b0cc4bfeb28884b
myjimnelson <jim@jimnelson.us>
2013-04-25 22:33:51 -0500

    hacked in some SVG iso tiles, needs lots of work

--- html/test.js
+++ html/test.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {          // when document ready...
 
+//var tileshape = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> <polygon points="0,31 31,0 63,31 31,63" style="fill:lime;stroke:purple;stroke-width:1"/> </svg>'
+
+//var tileshape = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> <polygon points="0,19 23,0 47,19 23,39" style="fill:lime;stroke:purple;stroke-width:1"/> </svg>'
+var tileshape = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1"> <polygon points="0,19 23,0 47,19 23,39"/> </svg>'
+
+//var tileshape = '<div class="isotile"><svg xmlns="http://www.w3.org/2000/svg" version="1.1"> <polygon points="0,31 31,0 63,31 31,63" style="fill:lime;stroke:purple;stroke-width:1"/> </svg></div>'
+
 var jsonmap = $.getJSON( "map.json", function() {
   console.log( "success" );
   })
   .done(function( data ) {
 
-      var table='<table>';
+      var mapwidth = data[0].length * 48
+      var rowheight = 20
+      var table='<div class="map">';
       /* loop over each object in the array to create rows*/
       $.each( data, function( index, item){
             /* add to html string started above*/
-        table+='<tr>';
-        if (index % 2 == 1) {table+='<td></td>'}
+        table+='<div class="maprow" width = "' + mapwidth.toString() + '">';
+        if (index % 2 == 1) {table+='<div></div>'}
         $.each( item, function( index, subitem){
           // table+='<td></td><td></td>';
-          table+='<td continent="' + subitem.continent+'">' + subitem.terrain+'</td><td></td>';
+          //table+='<td continent="' + subitem.continent+'">' + subitem.terrain+'</td><td></td>';
+          //table+='<td continent="' + subitem.continent+'"></td><td></td>';
+          table+='<div class="tile" continent="' + subitem.continent+'"></div><div></div>';
         });
-        if (index % 2 == 0) {table+='<td></td>'}
-        table+='</tr>';       
+        if (index % 2 == 0) {table+='<div></div>'}
+        table+='</div>';       
       });
-      table+='</table>';
+      table+='</div>';
 /* insert the html string*/
       $("#map").html( table );      
 
@@ -54,6 +65,8 @@ var jsonmap = $.getJSON( "map.json", function() {
   $("td[continent]").css("background-color", "green");
   $('td[continent="6"]').css("background-color", "blue");
 
+  //$("td[continent]").append(tileshape);
+  $("div.tile").append(tileshape);
   })
   .fail(function() { console.log( "error" ); })
   .always(function() { console.log( "complete" ); });
