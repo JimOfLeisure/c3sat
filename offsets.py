1fdb6e71ef3682a92552626bcc0e7026d1ecec81
myjimnelson <jim@jimnelson.us>
2015-01-02 22:34:59 +0000

    Minor updates. About to radically change civ3.py
    I'm pretty much going to wipe out civ3.py and start over reading
    predefined chunks in and inserting debug prints as I go.

---
+++ offsets.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+import sys
+from horspool import horspool    # to seek to first match; from http://inspirated.com/2010/06/19/using-boyer-moore-horspool-algorithm-on-file-streams-in-python
+
+def seek(saveStream, string):
+      reloffset = horspool.boyermoore_horspool(saveStream, string)
+      print string + " relative offset: " + str(reloffset)
+      return reloffset
+
+def main():
+    """Analyzing relative offsets between data structures"""
+    saveStream = sys.stdin
+    reloffset = 0
+    string="LEAD"
+    reloffset = seek(saveStream, "CNSL")
+    while reloffset >= 0 :
+      #reloffset = horspool.boyermoore_horspool(saveStream, string)
+      #print string + " relative offset: " + str(reloffset)
+      reloffset = seek(saveStream, "LEAD")
+      reloffset = seek(saveStream, "CULT")
+      reloffset = seek(saveStream, "CULT")
+    #saveStream.close()
+    return
+
+if __name__=="__main__":
+    main()
