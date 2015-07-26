a919a05a7d425726843121cc34f6895c8bbbb464
myjimnelson <jim@jimnelson.us>
2015-07-25 21:01:05 -0500

    Renamed main files for clarity

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -21,7 +21,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import wrld
+import ../civ3parse
 import urllib
 import subprocess
 import os
