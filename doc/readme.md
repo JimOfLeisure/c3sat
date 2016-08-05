96cccac29e5e80118300b3c161321fcf3e851d5b
myjimnelson <jim@jimnelson.us>
2016-08-04 20:57:42 -0500

    mostly doc and cleanup

--- doc/readme.md
+++ doc/readme.md
@@ -1,5 +1,7 @@
 August 2016: During the Go rewrite, taking more specific notes. They might as well go here.
 
+## Start of SAV file
+
 - "CIV3"
 - 26 bytes of data that is the same for all my saves but seems to be different for other versions and GOTM, COTM and LK154/CCM, so perhaps created from BIC affects this
 - "BIC "
@@ -10,4 +12,26 @@ August 2016: During the Go rewrite, taking more specific notes. They might as we
     - 4-byte zeroed int ?
     - 256-byte (?) string relative path to BIC/X/Q file
     - 4-byte zeroed int ?
-- "BICQVER#" C3C or "BICXVER#" PTW
+
+## BIC section of SAV file. Same as BIC/X/Q file?
+
+- "BICQVER#" C3C or "BICXVER#" PTW or "BIC VER#" vanilla
+- int, always 1. Count of BICs? Count of whatever the next blob is?
+- length, always 720 / 0x2d0
+- length bytes of data
+    - 0x00 - 0xff - four ints?
+    - 0x10 - string of well over 256 bytes - Description of scenario from BIQ
+        - COTM0120_OPEN
+        - COTM0121_OPEN
+        - GOTM0149_OPEN
+        - GOTM0150_OPEN
+        - Mesopotamia is the "cradle of civilization" and was home to all seven of the "Great Wonders of the Ancient World." The map stretches from the mountains of Greece east to the hills of Persia and south to the Nile river. The game ends as soon as all seven great wonders have been built, or when a side amasses 5500 VP, or after 160 turns -- whichever happens first. Victory points awarded for completing wonders are doubled in this scenario
+    - 0x290 - string - name of scenario
+        - CCM
+        - GOTM0150_OPEN
+        - GOTM
+        - COTM
+        - Mesopotamia
+    - All other seen bytes zeroes
+- "BLDG" in scenario-based saves, "GAME" in epic saves
+    - There must be an indicator in the file previous to this to indicate what data is present/missing
--- parseciv3/parseciv3.go
+++ parseciv3/parseciv3.go
@@ -63,70 +63,49 @@ func readBase(r *bytes.Reader) (c baseClass) {
 func somethingsdifferent(s string, r *bytes.Reader) {
 	// seeking backwards is causing EOF when I pass
 	// r.Seek(-4, 2)
-	log.Fatalf("%s\n%s\n", s, hex.Dump(readBytes(r, 1024)))
+	log.Fatalf("%s\n%s\n", s, hex.Dump(readBytes(r, 256)))
 }
 
 func readcivheader(r *bytes.Reader) {
 	civ3header := readBytes(r, 30)
 	_ = civ3header
-	/*
-	 * The first 30 bytes are identical on 151 saves on my PC, presumably all from c3c but some originally downloaded from CivFan
-	 * But I downloaded 2 GOTMs, 2 COTMs and LK154 SG (running CCM mod), and they all have different info from mine and from each other
-	 */
-	// civ3headerref := [30]byte{0x43, 0x49, 0x56, 0x33, 0x00, 0x1a, 0x18, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x7a, 0x1d, 0x96, 0xd6, 0x27, 0xca, 0x54, 0x4b, 0xa2, 0x76, 0x96, 0xd0, 0x81, 0x5d, 0x1a, 0xf7}
-	// for i := 0; i < len(civ3headerref); i++ {
-	// 	if civ3header[i] != civ3headerref[i] {
-	// 		log.Fatal(hex.Dump(civ3header))
-	// 	}
-	// }
-	// log.Println(hex.Dump(civ3header))
+
+	// Seems to be four or five ints followed by 2 strings, relative paths to bic resources and bic file
 	bicheader := readBase(r)
 	_ = bicheader
-	// log.Println(bicheader.name, bicheader.length)
-	// log.Println(hex.Dump(bicheader.buffer.Bytes()))
-
 }
 
 func readbic(r *bytes.Reader) {
-
-	// log.Println(hex.Dump(readBytes(r, 1024)))
-
-	// The next three values always seem to be the same in all files
-	// Best guess is that the 1 is a record count and 720 is a length,
-	// but almost all zeroes in the 720 bytes. But it does seem to lead
-	// up to the next class name "GAME" (or "BLDG" in scenario games)
 	bicqvernum := readBytes(r, 8)
 	switch string(bicqvernum) {
-	case "BICQVER#", "BICXVER#":
+	case "BICQVER#", "BIC VER#", "BICXVER#":
 	default:
 		log.Fatal(string(bicqvernum))
-		// log.Fatal(somethingsdifferent(r))
 		somethingsdifferent(string(bicqvernum), r)
 	}
+	// always seems to be 1
 	bicone := readBytes(r, 4)
 	if binary.LittleEndian.Uint32(bicone) != 1 {
 		somethingsdifferent(string(binary.LittleEndian.Uint32(bicone)), r)
 	}
-	homelesslength := int(binary.LittleEndian.Uint32(readBytes(r, 4)))
-	if homelesslength != 720 {
+	bicdescriptionlength := int(binary.LittleEndian.Uint32(readBytes(r, 4)))
+	if bicdescriptionlength != 720 {
 		somethingsdifferent(string(binary.LittleEndian.Uint32(bicone)), r)
 	}
-	homelessdata := readBytes(r, homelesslength)
-	_ = homelessdata
+	bicdescription := readBytes(r, bicdescriptionlength)
+	_ = bicdescription
+	log.Println(hex.Dump(bicdescription))
 
-	bicgame := readBytes(r, 4)
-	if string(bicgame) != "GAME" {
-		somethingsdifferent(string(bicgame), r)
-	}
+	// At this point, my epic SAVs have GAME, downloaded scenario-based games have BLDG
+
+	/*
+		bicgame := readBytes(r, 4)
+		if string(bicgame) != "GAME" {
+			somethingsdifferent(string(bicgame), r)
+		}
+	*/
 	// bicgame := readBase(r)
 	// log.Println(bicgame.name, hex.Dump(bicgame.buffer.Bytes()))
-
-	// Actually this does look like a list of integers, but it's more than 28
-
-	// Per Antal1987's Bic data structure, it looks like 28 ints then some other data is at the start of the BIC
-	// But the data doesn't seem to look like that
-	// twentyeightints := readBytes(r, 28*4)
-	// log.Println(hex.Dump(twentyeightints))
 
 	// log.Println(hex.Dump(readBytes(r, 1024)))
 
