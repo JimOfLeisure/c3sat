4d77e213d20538bd859d83ae5ed6bd6a6fa64276
Jim Nelson <jim@jimnelson.us>
2020-01-24 09:02:28 -0600

    added cia3.css and cia3-error

--- cmd/cia3/html/cia3.js
+++ cmd/cia3/html/cia3.js
@@ -1,4 +1,14 @@
-export class Filename extends HTMLElement {
+class Error extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        this.innerText = "Errors go here";
+    }
+}
+window.customElements.define('cia3-error', Error);
+
+class Filename extends HTMLElement {
     connectedCallback() {
         this.render()
     }
@@ -8,7 +18,7 @@ export class Filename extends HTMLElement {
 }
 window.customElements.define('cia3-filename', Filename);
 
-export class Difficulty extends HTMLElement {
+class Difficulty extends HTMLElement {
     connectedCallback() {
         this.render()
     }
