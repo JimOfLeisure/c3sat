987e2394d5ff7daba8f037883744356675ffdbae
Jim Nelson <jim@jimnelson.us>
2020-01-24 09:16:30 -0600

    added long polling to cia3.js

--- cmd/cia3/html/cia3.js
+++ cmd/cia3/html/cia3.js
@@ -1,3 +1,42 @@
+let pollXhr = new XMLHttpRequest();
+let pollSince = Date.now() - 86400000
+const longPollTimeout = 30
+
+let pollNow = () => {
+    pollXhr.open('GET', `/events?timeout=${longPollTimeout}&category=refresh&since_time=${pollSince}`);
+    pollXhr.send();
+}
+
+pollXhr.onload = () => {
+    if (pollXhr.status >= 200 & pollXhr.status < 300) {
+        let pollData = JSON.parse(pollXhr.responseText);
+        if (typeof pollData.events != 'undefined') {
+            pollSince = pollData.events[0].timestamp;
+            console.log('poll event received at ' + pollSince);
+        }
+        if (pollData.timeout != undefined) {
+            pollSince = pollData.timestamp;
+        }
+        pollNow();
+    } else {
+        // TODO: Better error handling. For now just passing the xhr object to a function which usually gets a ProgressEvent
+        // pollError(pollXhr);
+        console.log(pollXhr);
+    }
+}
+
+pollXhr.onerror = e => pollError(e);
+
+let pollError = (e) => {
+    console.error("Long poll returned error");
+    console.log(e);
+    const errorDiv = document.getElementsByTagName('cia3-error');
+    const errMsg = document.createElement('p');
+    errMsg.innerText = `Polling error. Live updates have stopped. Correct and refresh page.`
+    errorDiv[0].appendChild(errMsg);
+
+}
+
 class Error extends HTMLElement {
     connectedCallback() {
         this.render();
@@ -6,7 +45,6 @@ class Error extends HTMLElement {
         this.innerText = "Errors go here";
     }
 }
-window.customElements.define('cia3-error', Error);
 
 class Filename extends HTMLElement {
     connectedCallback() {
@@ -16,7 +54,6 @@ class Filename extends HTMLElement {
         this.innerText = "Filename goes here";
     }
 }
-window.customElements.define('cia3-filename', Filename);
 
 class Difficulty extends HTMLElement {
     connectedCallback() {
@@ -26,7 +63,6 @@ class Difficulty extends HTMLElement {
         this.innerText = "Difficulty goes here; warlord/emperor/etc";
     }
 }
-window.customElements.define('cia3-difficulty', Difficulty);
 
 const difficultyNames = [
     "Chieftan",
@@ -38,3 +74,8 @@ const difficultyNames = [
     "Deity",
     "Sid"
 ]
+
+window.customElements.define('cia3-error', Error);
+window.customElements.define('cia3-filename', Filename);
+window.customElements.define('cia3-difficulty', Difficulty);
+pollNow();
