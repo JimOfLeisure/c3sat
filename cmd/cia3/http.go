cbae88480a03f3a0c1b0e250601ab7e641f8b697
Jim Nelson <jim@jimnelson.us>
2020-04-02 19:50:20 -0500

    static mime headers fix

--- cmd/cia3/http.go
+++ cmd/cia3/http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/markbates/pkger"
 	"github.com/myjimnelson/c3sat/queryciv3"
@@ -35,13 +36,28 @@ func setHeaders(handler http.Handler) http.Handler {
 	})
 }
 
+// Set Content-Type explicitly since net/http FileServer seems to use Win registry which on many systems has wrong info for js and css in particular
+func setContentTypeHeaders(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		s := r.RequestURI
+		if len(s) > 3 && strings.ToLower(s[len(s)-3:]) == ".js" {
+			w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+		} else if len(s) > 4 && strings.ToLower(s[len(s)-4:]) == ".css" {
+			w.Header().Set("Content-Type", "text/css; charset=utf-8")
+		} else if len(s) > 5 && strings.ToLower(s[len(s)-5:]) == ".html" {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func server() {
 	gQlHandler, err := queryciv3.GraphQlHandler()
 	if err != nil {
 		log.Fatal(err)
 	}
 	staticFiles := http.FileServer(pkger.Dir("/cmd/cia3/html"))
-	http.Handle("/", staticFiles)
+	http.Handle("/", setContentTypeHeaders(staticFiles))
 	http.Handle("/graphql", setHeaders(gQlHandler))
 	http.Handle("/events", setHeaders(http.Handler(http.HandlerFunc(longPoll.SubscriptionHandler))))
 	err = http.ListenAndServe(addr+":"+httpPort, nil)
