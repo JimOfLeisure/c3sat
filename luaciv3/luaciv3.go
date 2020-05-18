d70c8a134669829783fab876f1fbced11ff9beba
Jim Nelson <jim@jimnelson.us>
2020-05-18 16:35:47 -0500

    memory limit

--- luaciv3/luaciv3.go
+++ luaciv3/luaciv3.go
@@ -6,9 +6,17 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
+// NewState is called to get a Lua environment with nbt manipulation ability// lua vm memory limit; 0 is no limit
+const memoryLimitMb = 100
+
 // NewState creates and returns a lua state with LuaCiv3 functions
+// TODO: memory limit?
 func NewState() *lua.LState {
 	L := lua.NewState()
+	// Set memory limit of lua instance (just a safety measure)
+	if memoryLimitMb > 0 {
+		L.SetMx(memoryLimitMb)
+	}
 	_ = LuaCiv3(L)
 	return L
 }
