4365a12329d7e85fa02d40403382a3eeaaed88f9
Jim Nelson <jim@jimnelson.us>
2020-05-18 14:33:37 -0500

    Corrected Blua -> LuaCiv3

--- luaciv3/luaciv3.go
+++ luaciv3/luaciv3.go
@@ -6,8 +6,8 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
-// Blua injects functions into a gopher-lua state
-func Blua(L *lua.LState) error {
+// LuaCiv3 injects functions into a gopher-lua state
+func LuaCiv3(L *lua.LState) error {
 	fmt.Println("luaciv3 doesn't do anything yet")
 	return nil
 }
