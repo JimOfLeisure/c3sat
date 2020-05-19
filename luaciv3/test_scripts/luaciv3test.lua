2ab01d0d38b9ce8ddbe0d513a0bb55e93fa7caf2
Jim Nelson <jim@jimnelson.us>
2020-05-19 12:53:16 -0500

    textmap.lua update

--- luaciv3/test_scripts/luaciv3test.lua
+++ luaciv3/test_scripts/luaciv3test.lua
@@ -1,30 +1,3 @@
-bic.load_default()
-sav.load(install_path .. "/Saves/Auto/Conquests Autosave 4000 BC.SAV")
-print(tile.width)
-print(tile.height)
-for k, v in ipairs(tile) do
-    -- print(v.terrain)
-    -- print(v.base_terrain)
-    -- print(v.overlay_terrain)
-
-    -- newline for end of map row
-    if (k - 1) % (tile.width / 2) == 0 then
-        io.write("\n")
-        -- indent odd map rows
-        if math.floor((k - 1) / (tile.width / 2)) % 2 == 1 then
-            io.write(" ")
-        end
-    end
-    -- tilde for water, blank for land
-    if v.base_terrain > 10 then
-        io.write("~ ")
-    else
-        io.write("  ")
-    end
-end
-io.write("\n")
-
-
 foo = get_savs({install_path .. "/Saves/Auto"})
 for _, v in pairs(foo) do
     print(v)
