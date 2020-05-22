dc4a871fdfb568dad151f71ff140538a381cd657
Jim Nelson <jim@jimnelson.us>
2020-05-22 18:58:12 -0500

    diff.go done

--- luaciv3/diff.go
+++ luaciv3/diff.go
@@ -1,8 +1,6 @@
 package luaciv3
 
 import (
-	"encoding/hex"
-
 	lua "github.com/yuin/gopher-lua"
 )
 
@@ -15,20 +13,20 @@ func diffModule(L *lua.LState) {
 		lt := L.NewTable()
 		diff.Append(lt)
 		L.RawSet(lt, lua.LString("name"), lua.LString(civString(currentBic.data[off:off+64])))
-		L.RawSet(lt, lua.LString("0"), lua.LNumber(currentBic.readInt32(off+68, Signed)))
-		L.RawSet(lt, lua.LString("1"), lua.LNumber(currentBic.readInt32(off+68, Signed)))
-		L.RawSet(lt, lua.LString("2"), lua.LNumber(currentBic.readInt32(off+72, Signed)))
-		L.RawSet(lt, lua.LString("3"), lua.LNumber(currentBic.readInt32(off+76, Signed)))
-		L.RawSet(lt, lua.LString("4"), lua.LNumber(currentBic.readInt32(off+80, Signed)))
-		L.RawSet(lt, lua.LString("5"), lua.LNumber(currentBic.readInt32(off+84, Signed)))
-		L.RawSet(lt, lua.LString("6"), lua.LNumber(currentBic.readInt32(off+88, Signed)))
-		L.RawSet(lt, lua.LString("7"), lua.LNumber(currentBic.readInt32(off+92, Signed)))
-		L.RawSet(lt, lua.LString("8"), lua.LNumber(currentBic.readInt32(off+96, Signed)))
-		L.RawSet(lt, lua.LString("9"), lua.LNumber(currentBic.readInt32(off+100, Signed)))
-		L.RawSet(lt, lua.LString("10"), lua.LNumber(currentBic.readInt32(off+104, Signed)))
-		L.RawSet(lt, lua.LString("11"), lua.LNumber(currentBic.readInt32(off+108, Signed)))
-		L.RawSet(lt, lua.LString("12"), lua.LNumber(currentBic.readInt32(off+112, Signed)))
-		L.RawSet(lt, lua.LString("13"), lua.LNumber(currentBic.readInt32(off+116, Signed)))
-		L.RawSet(lt, lua.LString("dump"), lua.LString("\n"+hex.Dump(currentBic.data[off:off+length])))
+		L.RawSet(lt, lua.LString("content_citizens"), lua.LNumber(currentBic.readInt32(off+68, Signed)))
+		L.RawSet(lt, lua.LString("max_anarchy_turns"), lua.LNumber(currentBic.readInt32(off+68, Signed)))
+		L.RawSet(lt, lua.LString("defense_land_units"), lua.LNumber(currentBic.readInt32(off+72, Signed)))
+		L.RawSet(lt, lua.LString("offense_land_units"), lua.LNumber(currentBic.readInt32(off+76, Signed)))
+		L.RawSet(lt, lua.LString("start_units_1"), lua.LNumber(currentBic.readInt32(off+80, Signed)))
+		L.RawSet(lt, lua.LString("start_units_2"), lua.LNumber(currentBic.readInt32(off+84, Signed)))
+		L.RawSet(lt, lua.LString("add_free_support"), lua.LNumber(currentBic.readInt32(off+88, Signed)))
+		L.RawSet(lt, lua.LString("bonus_each_city"), lua.LNumber(currentBic.readInt32(off+92, Signed)))
+		L.RawSet(lt, lua.LString("barb_attack_bonus"), lua.LNumber(currentBic.readInt32(off+96, Signed)))
+		L.RawSet(lt, lua.LString("cost_factor"), lua.LNumber(currentBic.readInt32(off+100, Signed)))
+		L.RawSet(lt, lua.LString("pct_optimal_cities"), lua.LNumber(currentBic.readInt32(off+104, Signed)))
+		L.RawSet(lt, lua.LString("ai_trade_rate"), lua.LNumber(currentBic.readInt32(off+108, Signed)))
+		L.RawSet(lt, lua.LString("corruption"), lua.LNumber(currentBic.readInt32(off+112, Signed)))
+		L.RawSet(lt, lua.LString("quelled_citizens"), lua.LNumber(currentBic.readInt32(off+116, Signed)))
+		// L.RawSet(lt, lua.LString("dump"), lua.LString("\n"+hex.Dump(currentBic.data[off:off+length])))
 	})
 }
